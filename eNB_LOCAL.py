7aed1e72668c3ca6188f68bea176999a4c655011
Fabricio <69990767+fasferraz@users.noreply.github.com>
2021-06-17 17:34:01 +0100

    Update eNB_LOCAL.py

--- eNB_LOCAL.py
+++ eNB_LOCAL.py
@@ -227,7 +227,7 @@ def return_plmn(mccmnc):
    if len(mccmnc)==5:
        return bcd(mccmnc[0] + mccmnc[1] + mccmnc[2] + 'f' + mccmnc[3] + mccmnc[4]) 
    elif len(mccmnc)==6:
-       return bcd(mccmnc[0] + mccmnc[1] + mccmnc[2] + mncmcc[5] + mccmnc[3] + mccmnc[4]) 
+       return bcd(mccmnc[0] + mccmnc[1] + mccmnc[2] + mccmnc[5] + mccmnc[3] + mccmnc[4]) 
    else:
      return b''
 
